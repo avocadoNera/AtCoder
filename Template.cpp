72b86560562480962a01cab048a7100af32a18ac
Nera <115917446+avocadoNera@users.noreply.github.com>
2025-05-23 07:23:27 +0900

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll   = long long;
+using vi   = vector<int>;
+using vll  = vector<ll>;
+using vvi  = vector<vi>;
+using vvll = vector<vll>;
+using pii  = pair<int,int>;
+using pic  = pair<int,char>;
+using vpii = vector<pii>;
+using vpic = vector<pic>;
+
+const int INF = 1e9; //1 * 10の9乗
+const ll  MOD = 998244353;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define repi(i, init, n) for (int i = init; i < (int)(n); i++)
+#define rrep(n, i) for (int i = n; i > 0; i--)
+#define rrepi(n, init, i) for (int i = n; i > init; i--)
+
+
+int main(){
+    
+}
